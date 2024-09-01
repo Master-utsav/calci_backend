a1d30f047e33a16318b3e412ee40ba7d232d2e5d
master-utsav <utsavjaiswal890@gmail.com>
2024-09-01 22:22:59 +0530

    fix_add

--- calciServer.js
+++ calciServer.js
@@ -8,7 +8,6 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 app.get('/calculate', (req, res) => {
     const expression = req.query.expression;
